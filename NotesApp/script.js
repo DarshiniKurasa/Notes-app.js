174d2dca6d0abf5c531dcaf40fe2e121ac9b6056
DarshiniKurasa <abhipriya.kurasa@gmail.com>
2024-12-18 23:12:37 +0530

    Update script.js

--- NotesApp/script.js
+++ NotesApp/script.js
@@ -1,5 +1,12 @@
 const notesContainer=document.querySelector(".notes-container");
 const createBtn=document.querySelector(".btn");
+function showNotes(){
+    notesContainer.innerHTML=localStorage.getItem("notes")
+}
+showNotes();
+function updateStorage(){
+    localStorage.setItem("notes",notesContainer.innerHTML);
+}
 
 createBtn.addEventListener("click", ()=>{
     let inputBox=document.createElement("p");
@@ -13,5 +20,17 @@ createBtn.addEventListener("click", ()=>{
 notesContainer.addEventListener("click",function(e){
     if(e.target.tagName==="IMG"){       //Case Sensitivity: The tagName property should be checked in uppercase ("IMG")
         e.target.parentElement.remove();
+        updateStorage();
+    }
+    else if(e.target.tagName==='P'){
+        notes=document.querySelectorAll(".input-box");
+        notes.forEach(nt =>nt.addEventListener("keyup",updateStorage));
     }
 });
+
+document.addEventListener("keydown",event=>{
+    if(event.key==="Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
+})
